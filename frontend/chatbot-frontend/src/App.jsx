f86e99d2049b6514efba3f873823bfd9b4bec9e8
ROLEX3013 <dhrpatel2833@gmail.com>
2025-08-22 19:22:24 +0530

    updated app.jsx and app.css file

--- frontend/chatbot-frontend/src/App.jsx
+++ frontend/chatbot-frontend/src/App.jsx
@@ -1,16 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Signup from "./pages/signup";
 import Login from "./pages/login";
 import Chat from "./pages/Chat";
-
+import "./App.css"
 function App() {
   return (
     <Router>
+    
+       <div className="navbar">
+  <Link to="/auth/signup">Signup</Link>
+  <Link to="/auth/login">Login</Link>
+  <Link to="/chat">Chat</Link>
+</div>
+
       <Routes>
-        <Route path="auth/signup" element={<Signup/>} />
-        <Route path="auth/login" element={<Login/>} />
-        <Route path="/chat" element={<Chat/>} />
+        <Route path="/auth/signup" element={<Signup />} />
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/chat" element={<Chat />} />
       </Routes>
     </Router>
   );
